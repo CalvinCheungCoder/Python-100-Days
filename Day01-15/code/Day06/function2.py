8fb82ce94b87877b5d38a20a59304389c6ca3bff
Calvin <984382258@qq.com>
2019-06-15 23:23:22 +0800

    day 06

---
+++ Day01-15/code/Day06/function2.py
@@ -0,0 +1,18 @@
+# 函数的定义和使用 - 求最大公约数和最小公倍数
+
+
+def gcd(x, y):
+    if x > y:
+        (x, y) = (y, x)
+    for factor in range(x, 1, -1):
+        if x % factor == 0 and y % factor == 0:
+            return factor
+    return 1
+
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+
+print(gcd(15, 27))
+print(lcm(15, 27))
