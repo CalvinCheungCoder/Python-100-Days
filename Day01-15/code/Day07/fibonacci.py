2b27c287d76da9dec1d580cb1efc4942dfda9102
Calvin <984382258@qq.com>
2019-06-16 23:02:11 +0800

    day 07

---
+++ Day01-15/code/Day07/fibonacci.py
@@ -0,0 +1,13 @@
+# 生成斐波拉切数列
+
+def main():
+    f = [1 , 1]
+    for i in range(2, 20):
+        f += [f[i - 1] + f[i - 2]]
+        # f.append(f[i - 1] + f[i - 2])
+    for val in f:
+        print(val, end=' ')
+
+
+if __name__ == '__main__':
+    main()
